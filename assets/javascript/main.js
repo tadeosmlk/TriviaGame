de6543b423366b5c9e495066c9847c3297b3ad18
Mimi Alemu <tadeosmlk@gmail.com>
2018-07-08 21:43:09 -0700

    initial commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,254 @@
+questions=
+[
+[{'what is quesion 01':{'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 02': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 03': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 04': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 05': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 06': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 07': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 08': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 09': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 010': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}}],
+[{'what is quesion 11':{'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 12': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 13': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 14': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 15': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 16': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 17': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 18': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 19': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 110': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}}],
+[{'what is quesion 21':{'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 22': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 23': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 24': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 25': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 26': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 27': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 28': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 29': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 210': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}}],
+[{'what is quesion 31':{'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 32': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 33': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 34': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 35': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 36': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 37': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 38': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 39': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 310': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}}],
+[{'what is quesion 41':{'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 42': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 43': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 44': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 45': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 46': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 47': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 48': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 49': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}},
+{'what s quesion 410': {'a': 'optiona', 'b': 'optionb', 'c': 'optionc', 'd': 'optiond', 'answer': 'optionc'}}]
+
+]
+
+pickedQuestions = []
+numberofQuestions = 10
+var timerflag = 0
+$('#start').on('click', startGame)
+// $('#submit').on('click', submitAnswers)
+
+var timeInSeconds = 5;
+
+function generateQuestionNumber(numberofQuestions)
+{
+  // debugger;
+  randNum = Math.floor(Math.random()*numberofQuestions)
+
+  if(pickedQuestions.indexOf(randNum) >= 0)
+  {
+    console.log(randNum +' is  picked already')
+    generateQuestionNumber(numberofQuestions)
+  }
+  else
+  {
+    pickedQuestions.push(randNum)
+    console.log(randNum)
+    return randNum
+  }
+}
+
+function starttimer()
+{
+  var countDownDate = new Date().getTime();
+  console.log(countDownDate)
+}
+//******************_
+function startTimer(duration) {
+  console.log('durariton is ' + duration);
+  console.log('timeInSeconds' + timeInSeconds)
+  timer = duration;
+
+  var timeIntervalID = setInterval(function () {
+      seconds = timer
+      timeInSeconds = seconds;
+      $('.timer').text('You have '+ timeInSeconds + 'seconds left. ')
+      console.log('timeInSeconds after a while: ' + timeInSeconds)
+      if(timerflag == 1)
+        {
+        console.log('timerflag == ' + timerflag)
+        clearInterval(timeIntervalID);
+        }
+      else{
+      if (--timer < 0) {
+
+      timer = 0;
+      if (timer == 0 ) {
+        timer = timeInSeconds;
+        submitAnswers('timer');
+        clearInterval(timeIntervalID);
+
+      }
+    }}
+  }, 1000);
+
+}
+
+//******************_
+function startGame(){
+
+    event.preventDefault();
+    timerflag = 0;
+    starttimer();
+    console.log(timerflag + ' timerflag')
+    console.log('---------------------')
+    console.log($(this).text())
+    $('#innerContainer').show()
+    $('#innerContainer').empty()
+    $('.timer').show()
+    $('#submit').show()
+    //$('.AllDonediv').empty()
+
+    if ($(this).text() == 'Start')
+    {
+        console.log('heerrreee')
+        startTimer(5)
+        console.log(timeInSeconds)
+        console.log('Starting...........')
+        k = generateQuestionNumber(4);
+        console.log('randNumber = ' + k)
+        for (var j = 0; j <10; j++)
+
+        {
+          console.log(questions[k][j])
+           questionIndex = j
+          for (key in questions[k][j])
+          {
+            console.log(key)
+            questionDiv = $('<div>').attr('class','question'+questionIndex)
+            $('#innerContainer').append(questionDiv.text(key))
+            var newdiv
+            for (var i = 0; i < 4; i++)
+            {
+              if(i==0)
+              {
+                choiceletter='a'
+              }
+              else if (i==1)
+              {
+                choiceletter='b'
+              }
+              else if (i==2)
+              {
+                choiceletter='c'
+              }
+              else if (i==3)
+              {
+                choiceletter='d'
+              }
+              // console.log(choiceletter)
+              newdiv = $('<span>')
+              choices = $('<input>').attr('type', 'radio').attr('id', i).attr('name', 'question'+questionIndex).attr('value', questions[k][questionIndex][key][choiceletter] )
+              // console.log('******' + questions[questionIndex][key][choiceletter])
+              choiceString = questions[k][questionIndex][key][choiceletter]
+              var newelemnt = $(newdiv).add(choices)
+              $('#innerContainer').append(newelemnt.text('\t\t\t\t'+questions[k][questionIndex][key][choiceletter]))
+            }
+
+          }
+
+        }
+        $('button').text('Submit').attr('id', 'submit')
+
+
+        }
+
+        // $('#submit').on('click', submitAnswers)
+        else if ($(this).text() == 'Submit')
+        {
+          console.log('subbbbmmmmiiiittteee')
+          submitAnswers('button')
+
+        }
+}
+
+function submitAnswers(flag){
+
+  if (flag == 'button')
+    {
+      event.preventDefault();
+
+      timerflag = 1;
+      clearInterval();
+      var slog = 'Greate, you Finished all questions on time. '
+    }
+  else
+  {
+    if(timerflag == 0)
+    var slog = 'Time is up.'
+  }
+  correctAnswers  = 0
+  incorrectAnswers = 0
+  unansweredQuestions = 0
+
+  doneDiv = $('<div>').attr('class','AllDonediv'+questionIndex)
+
+  for(var i = 0; i <10; i++)
+  {
+   quest = 'question'+i
+
+  var selectedAnswers = $("input[name="+ quest + "]:checked").val()
+  if(!selectedAnswers)
+  {
+    unansweredQuestions++
+  }
+  else
+  {
+  console.log(quest)
+  console.log(selectedAnswers)
+  for(key in questions[k][i])
+  {
+
+      if(selectedAnswers === questions[k][i][key]['answer'] )
+      {
+        correctAnswers++
+      }
+      else if (selectedAnswers === 'undefined')
+      {
+      unansweredQuestions++
+      }
+      else
+      {
+        incorrectAnswers++
+      }
+  }
+}
+}
+  var doneContent = $('.AllDonediv').append(doneDiv.html('<h1>Trivia Game</h1><p>'+slog+'</p><p>Correct Answers: '+correctAnswers+'<br> Incorrect Answers: '+incorrectAnswers+'<br> Unanswered Questions: '+unansweredQuestions ))//+ '<br><button> Start </button>'))
+
+  $('#innerContainer').hide()
+  $('.timer').hide()
+  $('#submit').hide()
+
+}
